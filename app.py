f06ec5dd3d3159731cb0f913dadc4e1b690b1099
bowingman <geohinster.sag@gmail.com>
2023-02-28 21:50:20 -0500

    adsf

--- app.py
+++ app.py
@@ -11,36 +11,36 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
 
-# URL = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
-# XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
-# TIMEOUT = 20
+URL = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
+XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
+TIMEOUT = 20
 
-# st.title("Test Selenium")
-# st.markdown(
-#     "You should see some random Football match text below in about 21 seconds")
+st.title("Test Selenium")
+st.markdown(
+    "You should see some random Football match text below in about 21 seconds")
 
-# firefoxOptions = Options()
-# firefoxOptions.add_argument("--headless")
-# service = Service(GeckoDriverManager().install())
-# driver = webdriver.Firefox(
-#     options=firefoxOptions,
-#     service=service,
-# )
-# driver.get(URL)
+firefoxOptions = Options()
+firefoxOptions.add_argument("--headless")
+service = Service(GeckoDriverManager().install())
+driver = webdriver.Firefox(
+    options=firefoxOptions,
+    service=service,
+)
+driver.get(URL)
 
-# try:
-#     WebDriverWait(driver, TIMEOUT).until(
-#         EC.visibility_of_element_located((By.XPATH, XPATH,))
-#     )
+try:
+    WebDriverWait(driver, TIMEOUT).until(
+        EC.visibility_of_element_located((By.XPATH, XPATH,))
+    )
 
-# except TimeoutException:
-#     st.warning("Timed out waiting for page to load")
-#     driver.quit()
+except TimeoutException:
+    st.warning("Timed out waiting for page to load")
+    driver.quit()
 
-# time.sleep(10)
-# elements = driver.find_elements(By.XPATH, XPATH)
-# st.write([el.text for el in elements])
-# driver.quit()
+time.sleep(10)
+elements = driver.find_elements(By.XPATH, XPATH)
+st.write([el.text for el in elements])
+driver.quit()
 
 
 def calc_num():
